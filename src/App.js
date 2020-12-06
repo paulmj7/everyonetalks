ee1a5b504b5f9514c78362ce700b80ea11fefa0d
Paul Jones <paulj51299@gmail.com>
2020-12-05 22:13:31 -0500

    source files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let circBuff = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2"];
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayText: [" "],
+      option: "0"
+    }
+    this.handleChangeLetter = this.handleChangeLetter.bind(this);
+    this.handleNextLetter = this.handleNextLetter.bind(this);
+  }
+  handleChangeLetter(event) {
+    event.preventDefault();
+    let letter = circBuff[0];
+    circBuff.shift();
+    circBuff.push(letter);
+    if (letter === "1") {
+      let tempText = this.state.displayText;
+      tempText.pop();
+      this.setState({ displayText: tempText, option: "1" });
+      window.speechSynthesis.speak(new SpeechSynthesisUtterance("DELETE LETTER"));
+    } else if (letter === "2") {
+      let tempText = this.state.displayText;
+      tempText.pop();
+      this.setState({ displayText: tempText, option: "2" });
+      window.speechSynthesis.speak(new SpeechSynthesisUtterance("CLEAR SCREEN"));
+    } else {
+      let tempText = this.state.displayText;
+      tempText[tempText.length-1] = letter;
+      this.setState({ displayText: tempText });
+      window.speechSynthesis.speak(new SpeechSynthesisUtterance(letter));
+    }
+  }
+  handleNextLetter(event) {
+    event.preventDefault();
+    if (this.state.option === "1") {
+      let delText = this.state.displayText;
+      delText[delText.length-1] = " ";
+      this.setState({ displayText: delText, option: "0" });
+      window.speechSynthesis.speak(new SpeechSynthesisUtterance("Success, current word is"));
+      window.speechSynthesis.speak(new SpeechSynthesisUtterance(delText.toString()));
+    } else if (this.state.option === "2") {
+      let clearText = [" "];
+      this.setState({ displayText: clearText, option: "0" });
+      window.speechSynthesis.speak(new SpeechSynthesisUtterance("Success, screen is cleared"));
+    } else {
+      let tempText = this.state.displayText;
+      tempText.push(" ");
+      this.setState({ displayText: tempText })
+      window.speechSynthesis.speak(new SpeechSynthesisUtterance(tempText.toString()));
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 className="textbar">{this.state.displayText}</h1>
+        <button className="button1" onClick={this.handleChangeLetter}>Letter</button>
+        <button className="button2" onClick={this.handleNextLetter}>Space</button>
+      </div>
+    )
+  }
 }
 
 export default App;
